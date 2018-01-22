74b514f016a10b1e5a21bdebcc917b7e895b4641
Unknown <thelordsnake@hotmail.com>
2018-01-21 21:37:24 -0600

    21/01/18

---
+++ botanical.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"> -->
+	<title>OSTEO SIN MAX</title>
+
+</head>
+<body>
+<?php include 'nav/barra.php';?>
+
+
+<div class="container ">
+	<div class="row mt-5" >
+			<div class="col-4 mt-3">
+				<div class="card card-outline-primary">
+					<img src="img/capsulas de corteza de neem.jpg" class="card-img-top img-fluid" id="fondo" alt="">		
+				</div>
+			</div>
+	
+			<div class="col-8 mt-3">
+				<div class="card " style="background-color:   #F39C12">
+					<div class="card-block ">
+						<h1 class=" card-title text-center">Botanical gel</h1>
+						<p class="card text-left">
+								<h4>Tratamiento auxilar:</h4>
+                                <p>es uno de los productos de mayor eficacia contra  
+                                mosquitos o insectos similares; además de aceite de neem y 
+                                aloe vera incorporamos citronela y aceite de geranio para 
+                                incrementar su efectividad. puede ser aplicado en niños. 
+                                muchas personas recuerdan aplicarse
+                                protección para mosquitos hasta después de ser picados,
+                                 en estos casos nuestro producto alivia y 
+                                desinflama esas picaduras además de que previene nuevos 
+                                ataques de mosquitos.
+                                <br>
+                                 <p>Cont. 240ml
+                
+                                
+
+
+						  
+						     <p class="card-title h3 text-center" >Resultados imediatos</p>					 
+						</p>
+					</div>
+				</div>
+			</div>
+	</div>
+</div>
+
+<div class="container ">
+	<div class="row mt-2 mb-2">
+		<div class="col">
+			<div class="card " style="background: #F1C40F  " >
+				<div class="card-block">
+						<h3 class="card-title text-center">Administración del medicamento</h3>
+						<p class=" text-center">
+						ESTE PRODUCTO NO ES UN MEDICAMENTO,
+                         ES UN COMPUESTO 
+                        NUTRITIVO Y FORMULADO CON PROPIEDADES
+                         100%NATURALES; ES RESPONSABILIDAD DE 
+                         QUIEN LO RECOMIENDA Y DE QUIEN LO USA. 
+						</p>
+
+				</div>
+			</div>
+		</div>
+			<div class="col ">
+				<div class="card " style="background: #F1C40F  " >
+					<div class="card-block">
+						<h3 class="card-title text-center">Precios</h3>
+						<p class=" card-text text-center"> 
+								1 A $135 <br>
+                                A partir de 5 $105
+								
+						</p>
+					</div>
+				</div>
+			</div>
+	</div>	
+</div>
+<div class="container">
+	<div class="row mt-3 mb-3">
+		<div class="col">
+			
+		</div>
+	</div>
+</div>
+
+
+<footer>
+	<div class="container ">
+		<div class="row">
+		
+			<div class="col">
+				<div class="card card-inverse mt-3" style="background: black" >
+					<div class="card-block">
+
+							<h3 class="card-title text-center">Contacto y Pedidos</h3>
+						<p class="card-text text-center">
+						222-810-49-55 <br>	
+						921-104-92-88 <br>
+						921-121-73-06 <br>	
+						921-207-47-50 <br>
+					</p>
+					</div>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card card-inverse  mt-3" style="background: black">
+					<div class="card-block">
+						<h3 class="card-title">Preguntas frecuentes</h3>
+						<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, quibusdam.</p>
+						<a href="#" class="btn " style="background: #F1C40F">Ir al post</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+<script src="js/headroom.min.js"></script>
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script> -->
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
